152603413f5acf3c2c3bcbe156a1f703144680e4
polkmetadatalib <30931127+polkmetadatalib@users.noreply.github.com>
2018-03-01 13:42:49 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='aspace',
-	version='1.0',
-	description='Client for working with REST API for ArchviesSpace',
+	version='1.3',
+	description='Client for working with REST API for ArchivesSpace',
 	url='https://github.com/polkmetadatalib/aspace'
 	author='Patrick Harrington'
 	author_email='harringp@uwosh.edu'
